1fd84c5fee1da8ce796fea02ae2fd46d1ad36a31
a.mangir <a.mangir@dextechnology.com>
2020-04-07 02:27:43 +0300

    settings project

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import styles from "./test.module.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <img src={logo} className="App-logo" alt="logo" /> <div></div>
         <a
           className="App-link"
           href="https://reactjs.org"
