9f618a343e11c763f7c658951f8253f6e07f7103
a.mangir <a.mangir@dextechnology.com>
2020-04-10 23:33:34 +0300

    Updates

---
+++ precache-manifest.fbdd5b3828bc0c2982713381ae003a2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d609bbd3339a5b237bb80d891760553d",
+    "url": "/farm-app/index.html"
+  },
+  {
+    "revision": "f51e6e0230e1d89f25fa",
+    "url": "/farm-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "eaa1fa889bbcb5ab416b",
+    "url": "/farm-app/static/js/2.4dbafed8.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/farm-app/static/js/2.4dbafed8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f51e6e0230e1d89f25fa",
+    "url": "/farm-app/static/js/main.f93c0df4.chunk.js"
+  },
+  {
+    "revision": "f2eae708cc58b6189d56",
+    "url": "/farm-app/static/js/runtime-main.78fbc112.js"
+  }
+]);
